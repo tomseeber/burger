4b3735fd2d1cc12809bda19c7c82aa003582c728
unknown <veggie_munchiesandcrunchies@hotmail.com>
2019-02-12 18:17:38 -0500

    db updated

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,9 @@ USE burgers_db;
 -- Create the table tasks.
 CREATE TABLE burgers
 (
-id int NOT NULL AUTO_INCREMENT,
+id int AUTO_INCREMENT NOT NULL,
 burger_name varchar(255) NOT NULL,
 devoured BOOLEAN DEFAULT false, 
+createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY (id)
 );
